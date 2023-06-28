668467186398e7874e11d85fa2df93c89cb75a27
Sanu Abraham <84289462+Sanu-Abraham@users.noreply.github.com>
2023-06-28 12:24:54 +0530

    Create BillingSystem.java

---
+++ BillingSystem.java
@@ -0,0 +1,11 @@
+public class BillingSystem {
+    public static void main (String[] args) {
+        
+        Menu m = new Menu();
+        m.init();
+        
+        Interface i = new Interface();
+        i.getInput();
+        
+    }
+}
